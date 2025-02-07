b5021929d0a2e6bb78507d569361ea876b44f0e5
toqduj <brian.pauw@bam.de>
2025-02-07 12:56:34 +0100

    get me some logging for now

--- LaudaProlineIOC.py
+++ LaudaProlineIOC.py
@@ -42,7 +42,7 @@ class LaudaClient:
         with self._connect() as sock:
             sock.sendall(message.encode('utf-8'))
             response = sock.recv(1024).decode('utf-8')
-        logging.info(f"Sent message: {message}, response: {response}")
+        logging.warn(f"Sent message: {message}, response: {response}")
         if response.startswith("ERR_"):
             logging.error(f"Error reading {group_command}: {response}")
             raise ValueError(f"{response}")
@@ -59,7 +59,7 @@ class LaudaClient:
         with self._connect() as sock:
             sock.sendall(message.encode('utf-8'))
             response = sock.recv(1024).decode('utf-8')
-        logging.info(f"Sent message: {message}, response: {response}")
+        logging.warn(f"Sent message: {message}, response: {response}")
         if not response.startswith("OK"):
             raise ValueError(f"Unexpected response: {response}")
 
